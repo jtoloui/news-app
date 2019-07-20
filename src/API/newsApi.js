7d203833eec2262e2168d86c2e8ddd0ad19676b4
Jamie Toloui <jamie@toloui.com>
2019-07-20 22:12:05 +0100

    changed base

--- src/API/newsApi.js
+++ src/API/newsApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import config from '../config';
-const KEY = config.API;
 
 export default axios.create({
-		baseURL: `https://newsapi.org/v2/top-headlines?apiKey=${KEY}&pageSize=40&sources=techcrunch, techradar, hacker-news, the-verge, wired`,
+		baseURL: config.HomeNews,
 	});
