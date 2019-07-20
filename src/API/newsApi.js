d1c8fa487d2126ab88797121b57fb9ae8a08c5af
Jamie Toloui <22939535+jtoloui@users.noreply.github.com>
2019-07-20 18:55:46 +0100

    changes

--- src/API/newsApi.js
+++ src/API/newsApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const KEY = "4902b5f3a0f34f279787e3215f86183f";
+const KEY = <YourAPIKEY>;
 
 export default axios.create({
 	baseURL: `https://newsapi.org/v2/top-headlines?category=technology&country=gb&country=us&apiKey=${KEY}&pageSize=40`,
