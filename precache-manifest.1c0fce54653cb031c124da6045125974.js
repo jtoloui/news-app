bf9c4ee2482dd1a0c799c8dcd3750041f17a0677
Jamie Toloui <jamie@toloui.com>
2019-07-20 18:34:42 +0100

    Updates

---
+++ precache-manifest.1c0fce54653cb031c124da6045125974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ae700346687aca17e93ca6eb13e45e",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "a927548b6cb76b11ddfc",
+    "url": "/news-app/static/js/2.e01583a0.chunk.js"
+  },
+  {
+    "revision": "f771878a69e235c83553",
+    "url": "/news-app/static/js/3.8fa0c90d.chunk.js"
+  },
+  {
+    "revision": "5c8509cc03582130b01f",
+    "url": "/news-app/static/js/4.7a54be9a.chunk.js"
+  },
+  {
+    "revision": "cfb35ba1ce0520f204da",
+    "url": "/news-app/static/js/main.52d9b450.chunk.js"
+  },
+  {
+    "revision": "aea489dc417ad3a08348",
+    "url": "/news-app/static/js/runtime~main.584ed6ff.js"
+  }
+]);
